36eb74272b3c84a70a5f16f5e3b94eaf66e456e1
gretly <gretly.gac@gmail.com>
2023-05-07 23:06:36 -0500

    ARCHIVOS PRIMER COMMIT

---
+++ operaciones.py
@@ -0,0 +1,24 @@
+n1=0
+n2=0
+suma=0
+resta=0
+multiplica=0
+divide=0
+
+print("BUEN DIA CLASE")
+
+print("INGRESE EL PRIMER NUMERO: ")
+n1=int(input())
+
+print("INGRESE EL SEGUNDO NUMERO: ")
+n2=int(input())
+
+suma = n1 + n2
+resta = n1 - n2
+multiplica = n1  * n2
+divide = n1 / n2
+
+print("LA SUMA ES: ", suma)
+print("LA RESTA ES: ", resta)
+print("LA MUKTIPLICAION ES: ", multiplica)
+print("LA DIVISION ES: ", divide)
